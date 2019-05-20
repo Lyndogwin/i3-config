c553b660b5b34183ed27f7209a557f905ab42f66
Lyndogwin <brand949@gmail.com>
2019-05-20 17:09:17 -0400

    successfully included config file and added script to create the appropriate symbolic links

---
+++ config-i3.sh
@@ -0,0 +1,14 @@
+# run commands to set up neccessary symbolic links
+ln -s ~/i3-config/i3 ~/.config/
+ln -s ~/i3-config/i3status ~/.config/
+ln -s ~/i3-config/compton.conf ~/.config/
+ln -s ~/i3-config/.zshrc ~/
+ln -s ~/i3-config/fonts ~/.local/share/
+ln -s ~/i3-config/.Xresources ~/
+ln -s ~/i3-config/.xsettingsd ~/ 
+ln -s ~/i3-config/.urxvt ~/ 
+ln -s ~/i3-config/.bash_aliases ~/ 
+#ln -s ~/i3-config/.xsettingsd ~/.config/ 
+#ln -s ~/i3-config/.xsettingsd ~/.config/ 
+#ln -s ~/i3-config/.xsettingsd ~/.config/ 
+
